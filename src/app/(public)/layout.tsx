abaad456eceb9b9ec7f39cca433d886a3afedcc7
ronniiii-i <ronniiii@yahoo.com>
2025-04-26 19:07:36 +0100

    refactor: comment out unused font import in layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,10 @@
 import "../globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import PublicNavbar from "@/components/navbar/PublicNavbar";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "ClaspCRM",
